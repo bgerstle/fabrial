21536856979adfbfb53276adaaafc9e6e50baac0
Brian Gerstle <brian.gerstle@gmail.com>
2018-08-28 16:44:02 -0500

    add failing test for listening on a port
    implements #3

---
+++ src/test/java/TcpClient.java
@@ -0,0 +1,34 @@
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
+
+public class TcpClient implements Closeable {
+  final String host;
+  final int port;
+  Optional<Socket> socket;
+
+  TcpClient(int port, Optional<String> host) {
+    this.host = host.orElse("localhost");
+    this.port = port;
+    this.socket = Optional.empty();
+  }
+
+  void connect(Optional<Integer> timeout) throws IOException {
+    assert !this.socket.isPresent();
+    Socket socket = new Socket();
+    this.socket = Optional.of(socket);
+    SocketAddress address = new InetSocketAddress(this.host, this.port);
+    socket.connect(address, timeout.orElse(0));
+  }
+
+  @Override
+  public void close() throws IOException {
+    if (this.socket.isPresent()) {
+      this.socket.get().close();
+    }
+  }
+}
