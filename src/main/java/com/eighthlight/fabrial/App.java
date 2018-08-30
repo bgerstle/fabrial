8e57cf516c6ed547376c39facdc862da4242fc34
Brian Gerstle <brian.gerstle@gmail.com>
2018-08-30 14:43:12 -0500

    Refactor App main to remove CPU-burning loop & static vars
    Global vars are bad, m'kay.

--- src/main/java/com/eighthlight/fabrial/App.java
+++ src/main/java/com/eighthlight/fabrial/App.java
@@ -4,44 +4,31 @@ import com.eighthlight.fabrial.server.ServerConfig;
 import com.eighthlight.fabrial.server.TcpServer;
 
 import java.io.IOException;
+import java.util.concurrent.Callable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class App {
   static final Logger logger = Logger.getLogger(App.class.getName());
 
-  static TcpServer server;
+  public static void main(String[] args) {
+    final TcpServer server = new TcpServer(new ServerConfig());
 
-  public static void start() {
-    server = new TcpServer(new ServerConfig());
     try {
       server.start();
     } catch (IOException e) {
       logger.log(Level.SEVERE, "Failed to start server", e);
       System.exit(1);
     }
-    Runtime.getRuntime().addShutdownHook(new Thread(App::shutdown));
-  }
 
-  public static void run() {
-    while (!server.isClosed()) {
-      // let server run
-    }
-  }
-
-  public static void shutdown() {
-    logger.info("Shutting down");
-    try {
-      server.close();
-    } catch (IOException e) {
-      logger.log(Level.SEVERE, "Failed to close server", e);
-      System.exit(1);
-    }
-  }
-
-  public static void main(String[] args) {
-    start();
-    run();
-    logger.info("Goodbye!");
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      logger.info("Shutting down");
+      try {
+        server.close();
+      } catch (IOException e) {
+        logger.log(Level.SEVERE, "Failed to close server", e);
+        System.exit(1);
+      }
+    }));
   }
 }
