81b25dc2c467784486f949048db65fee2aef6f96
Brian Gerstle <brian.gerstle@gmail.com>
2018-09-06 22:38:40 -0500

    Implement 200/404 behavior in FileHttpResponder

--- src/main/java/com/eighthlight/fabrial/http/FileHttpResponder.java
+++ src/main/java/com/eighthlight/fabrial/http/FileHttpResponder.java
@@ -1,8 +1,11 @@
 package com.eighthlight.fabrial.http;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Logger;
 
 public class FileHttpResponder implements HttpResponder {
+  private static final Logger logger = Logger.getLogger(FileHttpResponder.class.getName());
   private final FileResponderDataSource dataSource;
 
   public static interface FileResponderDataSource {
@@ -21,6 +24,11 @@ public class FileHttpResponder implements HttpResponder {
 
   @Override
   public Response getResponse(Request request) {
-    return new Response(HttpVersion.ONE_ONE, 501, null);
+    if (!request.method.equals(Method.HEAD)) {
+      return new Response(HttpVersion.ONE_ONE, 501, null);
+    }
+    boolean fileExists = dataSource.fileExistsAtPath(Paths.get(request.uri.getPath()));
+    int statusCode = fileExists ? 200 : 404;
+    return new Response(HttpVersion.ONE_ONE, statusCode, null);
   }
 }
