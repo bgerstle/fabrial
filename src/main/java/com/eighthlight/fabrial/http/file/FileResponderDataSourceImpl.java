6bd1f3fc2447b79800f562ff74a2b9345e2b8b91
Brian Gerstle <brian.gerstle@gmail.com>
2018-09-12 16:01:20 -0500

    Implement directory content handling, refactoring path expansion

--- src/main/java/com/eighthlight/fabrial/http/file/FileResponderDataSourceImpl.java
+++ src/main/java/com/eighthlight/fabrial/http/file/FileResponderDataSourceImpl.java
@@ -1,7 +1,9 @@
 package com.eighthlight.fabrial.http.file;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,19 +14,28 @@ public class FileResponderDataSourceImpl implements FileHttpResponder.DataSource
     this.baseDirPath = Optional.ofNullable(baseDirPath).orElse(Paths.get("."));
   }
 
+  private Path absolutePathInBaseDir(Path path) {
+    return Paths.get(baseDirPath.toAbsolutePath().toString(), path.toString());
+  }
+
   @Override
   public boolean fileExistsAtPath(Path path) {
-    return Paths.get(baseDirPath.toAbsolutePath().toString(), path.toString())
-                .toFile()
-                .exists();
+    return absolutePathInBaseDir(path).toFile().exists();
   }
 
   @Override
   public boolean isDirectory(Path path) {
-    return false;
+    return absolutePathInBaseDir(path).toFile().isDirectory();
   }
 
   @Override
   public List<Path> getDirectoryContents(Path path) {
+    return Arrays.asList(
+        Arrays.asList(absolutePathInBaseDir(path).toFile().listFiles())
+              .stream()
+              .map(File::getPath)
+              .map(Paths::get)
+              .map(baseDirPath::relativize)
+              .toArray(Path[]::new));
   }
 }
