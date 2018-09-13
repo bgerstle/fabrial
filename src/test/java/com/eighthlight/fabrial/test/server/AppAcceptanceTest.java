853e34c04b16a135bef76d8fe911272b615b28b2
Brian Gerstle <brian.gerstle@gmail.com>
2018-09-13 12:14:47 -0500

    Optimize headers, clean before running acceptance tests

--- src/test/java/com/eighthlight/fabrial/test/server/AppAcceptanceTest.java
+++ src/test/java/com/eighthlight/fabrial/test/server/AppAcceptanceTest.java
@@ -25,7 +25,6 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
 import static org.hamcrest.core.AllOf.allOf;
 
