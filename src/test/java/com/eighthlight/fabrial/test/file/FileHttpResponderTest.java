8ca3cd614b27a129cfc44e8cb6e7d5901b3c0002
Brian Gerstle <brian.gerstle@gmail.com>
2018-09-13 16:53:26 -0500

    Add generative test for GET arbitrary files

--- src/test/java/com/eighthlight/fabrial/test/file/FileHttpResponderTest.java
+++ src/test/java/com/eighthlight/fabrial/test/file/FileHttpResponderTest.java
@@ -18,6 +18,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
@@ -30,6 +31,7 @@ import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInA
 import static org.hamcrest.core.AllOf.allOf;
 import static org.quicktheories.QuickTheory.qt;
 import static org.quicktheories.generators.SourceDSL.lists;
+import static org.quicktheories.generators.SourceDSL.strings;
 
 public class FileHttpResponderTest {
   static final Gen<Path> filePaths() {
@@ -278,5 +280,48 @@ public class FileHttpResponderTest {
                   .map(is -> Result.attempt(is::readAllBytes).orElseAssert());
       assertThat(bodyBytes.orElse(null), is(data));
     }
+  }
+
+  @Test
+  void getArbitraryFileReturnsContents() {
+    qt().forAll(paths(32),
+                strings().allPossible().ofLengthBetween(1, 16).map(s -> s.getBytes()))
+        .checkAssert((relSubdirPath, data) -> {
+          // create temp directory
+          var baseDirFixture = new TempDirectoryFixture();
+          // setup folder inside arbitrary temp dir
+          var absSubdirPath = Paths.get(baseDirFixture.tempDirPath.toString(), relSubdirPath);
+          Result.attempt(() ->  Files.createDirectories(absSubdirPath))
+                .orElseAssert();
+          // create file in arbitrary temp dir subfolder
+          try (var tmpFileFixture = new TempFileFixture(absSubdirPath)) {
+            // write arbitrary data
+            tmpFileFixture.write(new ByteArrayInputStream(data));
+
+            var responder = new FileHttpResponder(
+                new FileResponderDataSourceImpl(baseDirFixture.tempDirPath));
+
+            var relFilePath =
+                baseDirFixture.tempDirPath.relativize(tmpFileFixture.tempFilePath).toString();
+
+            var response = responder.getResponse(
+                new RequestBuilder()
+                    .withVersion(HttpVersion.ONE_ONE)
+                    .withMethod(Method.GET)
+                    .withUriString(relFilePath)
+                    .build());
+
+            assertThat(response.statusCode, is(200));
+            assertThat(response.headers, hasEntry("Content-Length", Long.toString(data.length)));
+
+            var bodyBytes =
+                Optional.ofNullable(response.body)
+                        .map(is -> Result.attempt(is::readAllBytes).orElseAssert());
+            assertThat(bodyBytes.orElse(null), is(data));
+          } finally {
+            // close base temp directory
+            baseDirFixture.close();
+          }
+        });
   }
 }
