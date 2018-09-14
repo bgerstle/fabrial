274c946e671b93b3010460277460a90dd12c69a0
Brian Gerstle <brian.gerstle@gmail.com>
2018-09-14 13:10:47 -0500

    Remove mocked FileHttpResponder generative tests
    Preferring the simpler file-fixture-based tests.

--- src/test/java/com/eighthlight/fabrial/test/file/FileHttpResponderTest.java
+++ src/test/java/com/eighthlight/fabrial/test/file/FileHttpResponderTest.java
@@ -4,176 +4,140 @@ import com.eighthlight.fabrial.http.HttpVersion;
 import com.eighthlight.fabrial.http.Method;
 import com.eighthlight.fabrial.http.file.FileHttpResponder;
 import com.eighthlight.fabrial.http.file.FileResponderDataSourceImpl;
-import com.eighthlight.fabrial.http.request.Request;
 import com.eighthlight.fabrial.http.request.RequestBuilder;
-import com.eighthlight.fabrial.http.response.ResponseBuilder;
 import com.eighthlight.fabrial.utils.Result;
 import org.junit.jupiter.api.Test;
-import org.quicktheories.api.Subject1;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import static com.eighthlight.fabrial.test.http.ArbitraryHttp.paths;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
 import static org.hamcrest.core.AllOf.allOf;
 import static org.quicktheories.QuickTheory.qt;
 import static org.quicktheories.generators.Generate.pick;
-import static org.quicktheories.generators.SourceDSL.lists;
 import static org.quicktheories.generators.SourceDSL.strings;
 
 public class FileHttpResponderTest {
-  static final Subject1<List<HashSet<String>>> forAllListsOfExistingAndNonExistingFiles() {
-    return qt().forAll(lists().of(paths(32)).ofSizeBetween(1, 5),
-                       lists().of(paths(32)).ofSizeBetween(1, 5))
-               .as((paths1, paths2) -> {
-                 HashSet<String> ps1 = new HashSet<>(paths1);
-                 ps1.removeAll(paths2);
-                 HashSet<String> ps2 = new HashSet<>(paths2);
-                 ps2.removeAll(paths1);
-                 return List.of(ps1, ps2);
-               });
-  }
-
-  static FileHttpResponder responderForListOfExistingFiles(Set<String> files) {
-    return new FileHttpResponder(new FileHttpResponder.DataSource() {
-      @Override
-      public boolean fileExistsAtPath(String relPathStr) {
-        return files.contains(relPathStr);
-      }
-
-      @Override
-      public boolean isDirectory(String relPathStr) {
-        return false;
-      }
-
-      @Override
-      public List<String> getDirectoryContents(String relPathStr) {
-        return List.of();
-      }
-
-      @Override
-      public long getFileSize(String relPathStr) {
-        throw new IllegalCallerException();
-      }
-
-      @Override
-      public InputStream getFileContents(String relPathStr) {
-        throw new IllegalCallerException();
-      }
-
-      @Override
-      public String getFileMimeType(String relPathStr) {
-        return null;
-      }
-    });
-  }
-
-
   @Test
-  void responds200ToHeadForExistingFiles() {
-    forAllListsOfExistingAndNonExistingFiles()
-        .checkAssert((files) -> {
-          Set<String> existingFilePaths = files.get(0);
-          Set<String> nonExistingFilePaths = files.get(1);
-          FileHttpResponder responder = responderForListOfExistingFiles(existingFilePaths);
-          ArrayList<String> allFilePaths = new ArrayList<>();
-          allFilePaths.addAll(existingFilePaths);
-          allFilePaths.addAll(nonExistingFilePaths);
-          allFilePaths.forEach(p -> {
-            Request req = new Request(HttpVersion.ONE_ONE, Method.HEAD, Result.attempt(() -> new URI(p)).orElseAssert());
-            int expectedStatus = existingFilePaths.contains(p) ? 200 : 404;
-            assertThat(
-                responder.getResponse(req),
-                equalTo(new ResponseBuilder()
-                            .withVersion(HttpVersion.ONE_ONE)
-                            .withStatusCode(expectedStatus)
-                            .build()));
-          });
-        });
+  void headEmptyFile() {
+    try (var tmpFileFixture = new TempFileFixture()) {
+      var responder = new FileHttpResponder(
+          new FileResponderDataSourceImpl(tmpFileFixture.tempFilePath.getParent()));
+      var response = responder.getResponse(
+          new RequestBuilder()
+              .withVersion(HttpVersion.ONE_ONE)
+              .withMethod(Method.HEAD)
+              .withUriString(tmpFileFixture.tempFilePath.getFileName().toString())
+              .build());
+      assertThat(response.statusCode, is(200));
+      assertThat(response.headers, hasEntry("Content-Length", "0"));
+    }
   }
 
   @Test
-  void responds501ToUnsupportedMethodsOnExistingFiles() {
-    forAllListsOfExistingAndNonExistingFiles()
-        .checkAssert((files) -> {
-          Set<String> existingFilePaths = files.get(0);
-          Set<String> nonExistingFilePaths = files.get(1);
-          FileHttpResponder responder = responderForListOfExistingFiles(existingFilePaths);
-          existingFilePaths.forEach(p -> {
-            Request req =
-                new Request(HttpVersion.ONE_ONE,
-                            Method.DELETE,
-                            Result.attempt(() -> new URI(p.toString())).orElseAssert());
-            assertThat(
-                responder.getResponse(req),
-                equalTo(new ResponseBuilder()
-                            .withVersion(HttpVersion.ONE_ONE)
-                            .withStatusCode(501)
-                            .build()));
-          });
-        });
+  void headAbsentFileNotFound() {
+    try (var tmpDirFixture = new TempDirectoryFixture()) {
+      var responder = new FileHttpResponder(
+          new FileResponderDataSourceImpl(tmpDirFixture.tempDirPath));
+      var response = responder.getResponse(
+          new RequestBuilder()
+              .withVersion(HttpVersion.ONE_ONE)
+              .withMethod(Method.HEAD)
+              .withUriString("/foo")
+              .build());
+      assertThat(response.statusCode, is(404));
+      assertThat(response.headers, is(anEmptyMap()));
+      assertThat(response.body, is(nullValue()));
+    }
   }
 
   @Test
-  void responds404ToUnsupportedMethodsOnNonExistingFiles() {
-    forAllListsOfExistingAndNonExistingFiles()
-        .checkAssert((files) -> {
-          Set<String> existingFilePaths = files.get(0);
-          Set<String> nonExistingFilePaths = files.get(1);
-          FileHttpResponder responder = responderForListOfExistingFiles(existingFilePaths);
-          nonExistingFilePaths.forEach(p -> {
-            Request req =
-                new Request(HttpVersion.ONE_ONE,
-                            Method.DELETE,
-                            Result.attempt(() -> new URI(p.toString())).orElseAssert());
-            assertThat(
-                responder.getResponse(req),
-                equalTo(new ResponseBuilder()
-                            .withVersion(HttpVersion.ONE_ONE)
-                            .withStatusCode(404)
-                            .build()));
-          });
-        });
+  void headTextFile() {
+    try (var tmpFileFixture = new TempFileFixture(Paths.get("/tmp"), ".txt")) {
+      var data = "foo".getBytes();
+      tmpFileFixture.write(new ByteArrayInputStream(data));
+      var responder = new FileHttpResponder(
+          new FileResponderDataSourceImpl(tmpFileFixture.tempFilePath.getParent()));
+      var response = responder.getResponse(
+          new RequestBuilder()
+              .withVersion(HttpVersion.ONE_ONE)
+              .withMethod(Method.HEAD)
+              .withUriString(tmpFileFixture.tempFilePath.getFileName().toString())
+              .build());
+      assertThat(response.statusCode, is(200));
+      assertThat(response.headers, hasEntry("Content-Length", Long.toString(data.length)));
+      assertThat(response.headers, hasEntry("Content-Type", "text/plain"));
+    }
   }
 
   @Test
-  void responds200WithAllowToOptionsOnAnyPath() {
-    forAllListsOfExistingAndNonExistingFiles()
-        .checkAssert((files) -> {
-          Set<String> existingFilePaths = files.get(0);
-          Set<String> nonExistingFilePaths = files.get(1);
-          FileHttpResponder responder = responderForListOfExistingFiles(existingFilePaths);
-          ArrayList<String> allFilePaths = new ArrayList<>();
-          allFilePaths.addAll(existingFilePaths);
-          allFilePaths.addAll(nonExistingFilePaths);
-          allFilePaths.forEach(p -> {
-            Request req =
-                new Request(HttpVersion.ONE_ONE,
-                            Method.OPTIONS,
-                            Result.attempt(() -> new URI(p.toString())).orElseAssert());
-            var resp = responder.getResponse(req);
-            assertThat(resp.version, equalTo(HttpVersion.ONE_ONE));
-            assertThat(resp.statusCode, equalTo(200));
-            assertThat(resp.reason, nullValue());
-            assertThat(Arrays.asList(resp.headers.get("Allow").split(", ")),
-                       allOf(
-                           containsInAnyOrder("GET", "HEAD", "OPTIONS", "PUT", "DELETE"),
-                           not(contains("POST"))));
-            assertThat(resp.headers.get("Content-Length"),
-                       is("0"));
-          });
-        });
+  void unsupportedMethodOnExistingFile() {
+    try (var tmpFileFixture = new TempFileFixture()) {
+      var responder = new FileHttpResponder(
+          new FileResponderDataSourceImpl(tmpFileFixture.tempFilePath.getParent()));
+      var response = responder.getResponse(
+          new RequestBuilder()
+              .withVersion(HttpVersion.ONE_ONE)
+              .withMethod(Method.POST)
+              .withUriString(tmpFileFixture.tempFilePath.getFileName().toString())
+              .build());
+      assertThat(response.statusCode, is(405));
+      assertThat(response.headers, is(anEmptyMap()));
+      assertThat(response.body, is(nullValue()));
+    }
+  }
+
+  @Test
+  void unsupportedMethodOnAbsentFile() {
+    try (var tmpDirFixture = new TempDirectoryFixture()) {
+      var responder = new FileHttpResponder(
+          new FileResponderDataSourceImpl(tmpDirFixture.tempDirPath));
+      var response = responder.getResponse(
+          new RequestBuilder()
+              .withVersion(HttpVersion.ONE_ONE)
+              .withMethod(Method.POST)
+              .withUriString("/foo")
+              .build());
+      assertThat(response.statusCode, is(404));
+      assertThat(response.headers, is(anEmptyMap()));
+      assertThat(response.body, is(nullValue()));
+    }
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"/", "/foo"})
+  void optionsOnAbsentOrExistingFile(String path) {
+    try (var tmpDirFixture = new TempDirectoryFixture()) {
+      var responder = new FileHttpResponder(
+          new FileResponderDataSourceImpl(tmpDirFixture.tempDirPath));
+      var response = responder.getResponse(
+          new RequestBuilder()
+              .withVersion(HttpVersion.ONE_ONE)
+              .withMethod(Method.OPTIONS)
+              .withUriString(path)
+              .build());
+      assertThat(response.statusCode, is(200));
+      assertThat(response.headers, allOf(
+          hasEntry(is("Allow"), allOf(containsString("GET"),
+                                      containsString("DELETE"),
+                                      containsString("PUT"),
+                                      containsString("HEAD"),
+                                      containsString("OPTIONS"))),
+          hasEntry("Content-Length", "0")));
+      assertThat(response.body, is(nullValue()));
+    }
   }
 
   @Test
@@ -252,6 +216,7 @@ public class FileHttpResponderTest {
               .build());
       assertThat(response.statusCode, is(200));
       assertThat(response.headers, hasEntry("Content-Length", "0"));
+      assertThat(response.body, is(nullValue()));
     }
   }
 
