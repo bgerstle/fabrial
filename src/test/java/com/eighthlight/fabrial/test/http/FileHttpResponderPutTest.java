1d684f71c821ef44f3c2665b826d188d08b566a5
Brian Gerstle <brian.gerstle@gmail.com>
2018-09-20 16:18:03 -0500

    Add failing create file test

--- src/test/java/com/eighthlight/fabrial/test/http/FileHttpResponderPutTest.java
+++ src/test/java/com/eighthlight/fabrial/test/http/FileHttpResponderPutTest.java
@@ -4,9 +4,11 @@ import com.eighthlight.fabrial.http.HttpVersion;
 import com.eighthlight.fabrial.http.Method;
 import com.eighthlight.fabrial.http.file.FileHttpResponder;
 import com.eighthlight.fabrial.http.request.RequestBuilder;
+import com.eighthlight.fabrial.utils.Result;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.util.Map;
 
 import static java.util.Collections.emptyMap;
 import static org.hamcrest.CoreMatchers.is;
@@ -48,5 +50,32 @@ public class FileHttpResponderPutTest {
             .build());
     assertThat(response.statusCode, is(411));
     assertThat(response.headers, is(emptyMap()));
+  }
+
+  @Test
+  void putCreateFile() {
+    var mockFC = new MockFileController();
+    var responder = new FileHttpResponder(mockFC);
+
+    mockFC.root = new MockDirectory("foo");
+
+    var createdFilename = "baz";
+
+    var data = "buz".getBytes();
+
+    var response = responder.getResponse(
+        new RequestBuilder()
+            .withVersion(HttpVersion.ONE_ONE)
+            .withMethod(Method.PUT)
+            .withUriString(createdFilename)
+            .withBody(new ByteArrayInputStream(data))
+            .withHeaders(Map.of(
+                "Content-Length", Integer.toString(data.length)
+            ))
+            .build());
+    assertThat(response.statusCode, is(201));
+    assertThat(response.headers, is(emptyMap()));
+
+    assertThat(Result.attempt(() -> mockFC.getFileContents(createdFilename)), is(data));
   }
 }
