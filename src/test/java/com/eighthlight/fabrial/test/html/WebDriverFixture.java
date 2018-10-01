b6de2bc4e11235b7ec7262b659c4e5c47e5f2732
Brian Gerstle <brian.gerstle@gmail.com>
2018-10-01 15:28:42 -0500

    Add --no-sandbox flag to try to get chromium-chromedriver worker
    Following some recommendations from https://github.com/travis-ci/travis-ci/issues/7150

--- src/test/java/com/eighthlight/fabrial/test/html/WebDriverFixture.java
+++ src/test/java/com/eighthlight/fabrial/test/html/WebDriverFixture.java
@@ -2,12 +2,15 @@ package com.eighthlight.fabrial.test.html;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebDriverFixture implements AutoCloseable {
   public final WebDriver driver;
 
   public static WebDriverFixture chrome() {
-    return new WebDriverFixture(new ChromeDriver());
+    var opts = new ChromeOptions();
+    opts.addArguments("--no-sandbox");
+    return new WebDriverFixture(new ChromeDriver(opts));
   }
 
   public WebDriverFixture(WebDriver driver) {
