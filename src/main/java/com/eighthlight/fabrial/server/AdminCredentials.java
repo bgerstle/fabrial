0e1ec814882accc5e2cdfa9759e33c6d0e473e8f
Brian Gerstle <brian.gerstle@gmail.com>
2018-12-12 21:04:55 -0600

    Remove unnecessary Result usage

--- src/main/java/com/eighthlight/fabrial/server/AdminCredentials.java
+++ src/main/java/com/eighthlight/fabrial/server/AdminCredentials.java
@@ -10,15 +10,11 @@ public class AdminCredentials {
   public static final String PASSWORD_ENV_VAR = "ADMIN_PASSWORD";
 
   public static Optional<Credential> fromEnvironment(Map<String,String> env) {
-    return Result
-        .attempt(() -> {
-          return Optional
-              .ofNullable(env.get(USER_ENV_VAR))
-              .flatMap(user -> {
-                return Optional.ofNullable(env.get(PASSWORD_ENV_VAR))
-                               .map(pw -> new Credential(user, pw));
-              });
-        })
-        .orElseThrow();
+    return Optional
+        .ofNullable(env.get(USER_ENV_VAR))
+        .flatMap(user -> {
+          return Optional.ofNullable(env.get(PASSWORD_ENV_VAR))
+                         .map(pw -> new Credential(user, pw));
+        });
   }
 }
